879ee15ec1b358436776d2107f842035489f5996
Edwin U Kannanaikkal <edwinforedwin@gmail.com>
2025-08-28 09:40:14 +0530

    project componization

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,11 @@
 import { Routes } from '@angular/router';
-import { Login } from './components/login/login';
+import { Login } from './pages/login/login';
+import { Home } from './pages/home/home';
 
 export const routes: Routes = [
-    {path:'login', component:Login},
-    {path:'**', redirectTo:'login'}
+    {path:'app', children:[
+        {path:'login', component:Login},
+        {path:'home', component:Home}
+    ]},
+    {path:'**', redirectTo:'app/home'}
 ];
